bd59f00930a7f46b910d7421dfa3161488379645
Ping Lin <limphoebe8@gmail.com>
2024-02-07 15:38:01 +0000

    Start up.

---
+++ movies/src/main/java/dev/phoebe/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.phoebe.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
