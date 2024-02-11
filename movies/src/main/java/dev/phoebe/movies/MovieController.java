3577863d3c65198cad39082329569237a7a8d979
Ping Lin <limphoebe8@gmail.com>
2024-02-11 16:01:28 +0000

    App.js successfully gets movie data.

--- movies/src/main/java/dev/phoebe/movies/MovieController.java
+++ movies/src/main/java/dev/phoebe/movies/MovieController.java
@@ -4,10 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +14,7 @@ import java.util.Optional;
 /*A REST API controller is a component in a software application that handles incoming HTTP
 requests and provides responses according to the principles of Representational State Transfer (REST)*/
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     //initiate movieService
